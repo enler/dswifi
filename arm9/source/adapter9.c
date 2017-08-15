72e77076e811f11180a4ece0299bf45aad678041
enler <enler@live.cn>
2017-08-15 16:08:16 +0800

    add license to adapter7.c adapter7.h adapter9.c adapter9.h

--- arm9/source/adapter9.c
+++ arm9/source/adapter9.c
@@ -1,3 +1,24 @@
+/*
+adapter9.c -- bridge dswifi with nitrosdk
+adapter9.c is licenced under the MIT open source licence (same as dswifi):
+
+Copyright (C) 2017 enler (enler2011@gmail.com)
+
+Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), 
+to deal in the Software without restriction, including without limitation the rights to use, 
+copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, 
+and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, 
+INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR 
+PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, 
+DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, 
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+
+*/
+
 #include "nds/ndstypes.h"
 #include "adapter9.h"
 
@@ -35,7 +56,7 @@ void irqEnable(u32 irq) {
 
 void irqSet(u32 irq, VoidFn handler) {
 	OS_SetIrqFunction(irq, handler);
-	*(VoidFn*)(0x27E0018 + 0x28) = handler;
+	*(VoidFn*)(0x27E0018 + 0x28) = handler;//is some case (for example, nitrosdk ver <= 4.0), use 0x27E0000 instead
 }
 
 static inline u32 uncacheAddr(u32 address)
--- arm9/source/adapter9.h
+++ arm9/source/adapter9.h
@@ -1,3 +1,24 @@
+/*
+adapter9.h -- bridge dswifi with nitrosdk
+adapter9.h is licenced under the MIT open source licence (same as dswifi):
+
+Copyright (C) 2017 enler (enler2011@gmail.com)
+
+Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), 
+to deal in the Software without restriction, including without limitation the rights to use, 
+copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, 
+and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, 
+INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR 
+PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, 
+DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, 
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+
+*/
+
 typedef void (* VoidFn)(void);
 typedef void (*FifoAddressHandlerFunc)(void * address, void * userdata);
 typedef void (*FifoValue32HandlerFunc)(u32 value32, void * userdata);
